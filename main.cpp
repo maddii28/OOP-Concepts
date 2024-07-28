38ab09681dd5f8df1338970bfa47265ffa4b9811
Maddhav Suneja <ms14565@nyu.edu>
2024-07-28 20:07:33 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+
+int main() {
+    //This is going to print "Helloe World" to the console
+    std::cout << "Maddhav is the best" << std::endl; 
+
+    int value = 7/0; 
+    return 0;
+}
