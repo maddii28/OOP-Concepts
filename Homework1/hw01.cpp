80938fce9b8c2ae03a6eb23b52def2741431acb5
Maddhav Suneja <ms14565@nyu.edu>
2024-09-10 23:35:00 -0400

    daily homework commit

--- Homework1/hw01.cpp
+++ Homework1/hw01.cpp
@@ -5,24 +5,40 @@
 using namespace std;
 
 char decyph(char& encryp_char, int rotat_dist) {
-    if ('A' < encryp_char && encryp_char>'Z') {
+    if ('A' <= encryp_char && encryp_char <= 'Z') {
         encryp_char-=rotat_dist;
+        if ('A' > encryp_char) {
+            encryp_char+= 26;
+        }
+        if (encryp_char > 'Z') {
+            encryp_char-=26;
+        }
     }
     return encryp_char;
 }
 
 void str_decyph(string& encrypt_str, int rotat_dist) {
-    for (string& x: encrypt_str) {
+    for (char& x: encrypt_str) {
         decyph(x, rotat_dist);
     }
 }
 
  int main() {
-    string code_msg;
+    vector<string> vec_line;
+    string line;
     ifstream ifs;
+
     ifs.open("encrypted.txt");
-    while (ifs >> code_msg) {
-        str_decyph()
+
+    while (getline(ifs,line)) {
+        vec_line.push_back(line);
     }
 
+    int num = stoi(vec_line[0]); 
+
+    for (size_t i = vec_line.size()-1; i > 0; --i) {
+        str_decyph(vec_line[i], num);
+        cout << vec_line[i] << endl;
+    }
  }
+    
