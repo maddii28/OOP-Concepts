a42683e9ed64a80b450a334329beb486d456df2a
Maddhav Suneja <ms14565@nyu.edu>
2024-09-12 18:56:57 -0400

    daily homework commit

--- Homework1/hw01.cpp
+++ Homework1/hw01.cpp
@@ -1,4 +1,7 @@
-// NAME: MADDHAV SUNEJA
+/*
+NAME: MADDHAV SUNEJA
+PURPOSE: THE PROGRAM DECRYPTS A CAESER CIPHER ENCRYPTED FILE USING C++ BASICS.
+*/
 
 #include <iostream>
 #include <fstream>
@@ -6,52 +9,74 @@
 #include <vector>
 using namespace std;
 
-char decyph(char& encryp_char, int rotat_dist);
 
-void str_decyph(string& encrypt_str, int rotat_dist);
+char decypher(char& encrypted_letter, int rotate_value);
 
+void line_decypher(string& encrypt_sentence, int rotate_value);
+
+//main function includes the whole functional code.
 int main() {
-    vector<string> vec_line;
-    string line;
+
+    //Defining the various elements i.e vectors and variables
+    vector<string> lines;
+    int rotation;
+    string current_line;
+    string gobble;
     ifstream ifs;
 
+    //opening the encrypted file using defined variable ifs
     ifs.open("encrypted.txt");
 
-    while (getline(ifs,line)) {
-        vec_line.push_back(line);
+    //checking if file opened correctly
+    if (!ifs) {
+        cout << "Can't open the file";
+        exit(1);
     }
 
-    int num = stoi(vec_line[0]); 
+    //storing the rotation value from the first line in an int variable
+    ifs >> rotation;
 
-    for (size_t i = vec_line.size()-1; i > 0; --i) {
-        str_decyph(vec_line[i], num);
-        cout << vec_line[i] << endl;
+    //gobbling up the newline character before reading the encrypted txt
+    getline(ifs,gobble);
+
+    //Filling up the encrypted text in the vector using push_back method.
+    while (getline(ifs, current_line)) {
+        lines.push_back(current_line);
+    }
+
+    //Closing the encrypted file after reading
+    ifs.close();
+
+    //Looping over the vector- decrypting and printing it.
+    for (int row = lines.size()-1; row >= 0; --row) {
+        line_decypher(lines[row], rotation);
+        cout << lines[row] << endl;
     }
  }
    
-char decyph(char& encryp_char, int rotat_dist) {
-    if ('A' <= encryp_char && encryp_char <= 'Z') {
-        encryp_char-=rotat_dist;
-        if ('A' > encryp_char) {
-            encryp_char+= 26;
+//decyph function decrypts the character using the rotation value.
+char decypher(char& encrypted_letter, const int rotate_value) {
+
+    //makes sure only uppercase are decrypted
+    if ('A' <= encrypted_letter && encrypted_letter <= 'Z') {  
+        encrypted_letter -= rotate_value;
+
+        //the following condition considers the upper and lower bounds
+        if ('A' > encrypted_letter) {
+            encrypted_letter += 26;
         }
-        if (encryp_char > 'Z') {
-            encryp_char -= 26;
+        if (encrypted_letter > 'Z') {
+            encrypted_letter -= 26;
         }
     }
-    return encryp_char;
+    return encrypted_letter;
 }
 
-void str_decyph(string& encrypt_str, int rotat_dist) {
-    for (char& x: encrypt_str) {
-        decyph(x, rotat_dist);
+//str_decypher function decrypts a whole string using the decypher function.
+void line_decypher(string& encrypted_sentence, const int rotate_value) {
+    for (char& traverse: encrypted_sentence) {
+        decypher(traverse, rotate_value);
     }
 }
-
-/* Mistakes I made during the homework
-1. Mistake in the for condition for the decypher code - messed up the logical operators
-2. Type mismatch btw char and string for function call to decyph 
-3. i >= 0 was a wrong condition to loop over the vector backwards idk why??
-4. Forgot to consider the domain of the program - for eg. rotating alphabets on the edge - line 35-39 */
 
 
